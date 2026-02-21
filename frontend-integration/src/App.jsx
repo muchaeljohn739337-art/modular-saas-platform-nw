d10712f3813c2636c118421ad487759ca705769c
pdtribe181 <pdtribe181@gmail.com>
2026-02-21 15:13:32 -0500

    Fix Node.js version for Vercel deployment

---
+++ frontend-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HealthcareAIDashboard from './components/HealthcareAIDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <HealthcareAIDashboard />
+    </div>
+  );
+}
+
+export default App;
